8f77ca9f9b2817e39d5576045eb56d225d451fb4
janaya <julia.anaya@gmail.com>
2011-06-08 17:29:41 +0100

    Added debug and client test

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ var SubscriptionStore = function() {
 // Main PubSubHubub method. Peforms the subscription and unsubscriptions
 // It uses the credentials defined earlier.
 var subscribe = function(feed, mode, hub, callback, errback) {
+  log("Called subscribe");
   var params = {
     "hub.mode"      : mode,
     "hub.verify"    : config.pubsubhubbub.verify_mode,
@@ -125,26 +126,31 @@ var subscribe = function(feed, mode, hub, callback, errback) {
       contentLength = body.length,
       headers = {
         "Accept": '*/*',
-        "Authorization": "Basic "+ base64.encode(config.pubsubhubbub.username + ":" + config.pubsubhubbub.password),
+//        "Authorization": "Basic "+ base64.encode(config.pubsubhubbub.username + ":" + config.pubsubhubbub.password),
         "Content-Length": contentLength,
         "Content-Type": "application/x-www-form-urlencoded",
         "Host": hub.hostname,
         "User-Agent": "Socket-Sub for Node.js",
         "Connection": "close"
       };
-
+  log("request body: " + body);
   var client  = http.createClient(hub.port || 80, hub.hostname );
   var request = client.request("POST", hub.pathname + (hub.search || ""), headers);
-
+  log("hub.pathname: " + hub.pathname)
   request.write(body, 'utf8');
 
   request.addListener("response", function(response) {
+    log("in request.addListener response");
     var body = "";
     response.addListener("data", function(chunk) {
         body += chunk;
+        log("in request.addListener data");
+        log("body: " + body);
     });
     response.addListener('end', function() {
-      if(response.statusCode == 204) {
+      log("in request.addListener end");
+      if(response.statusCode == (202 || 204)) {
+        log("response.statusCode: " + response.statusCode);
         callback();
       }
       else {
@@ -162,6 +168,12 @@ var subscribe = function(feed, mode, hub, callback, errback) {
 
 // Web Socket Server ---- (server <-> browser) --------------------------------------
 var ws_server = ws.createServer({ debug: config.debug });
+var store  = new express.session.MemoryStore;
+ws_server.use(express.cookieParser());
+ws_server.use(express.session({ secret: 'something', store: store }));
+//var RedisStore = require('connect-redis');
+//app.use(express.cookieParser());
+//app.use(express.session({ secret: "keyboard cat", store: new RedisStore }));
 
 ws_server.addListener("listening", function() {
   var hostInfo = config.websocket.listen.host + ":" + config.websocket.listen.port.toString();
@@ -171,18 +183,28 @@ ws_server.addListener("listening", function() {
 // Handle Web Sockets when they connect
 ws_server.addListener("connection", function(socket ) {
   // When connected
+  log(socket.id);
   ws_server.send(socket.id, "Awaiting feed subscription request");
+  
+   //socket.once('message', function(sid) {
+   // store.get(sid, function(err, session) {
+   //   if (err || !session) {
+        // Do some error handling, bail.
+   //     return;
+   //   }
+  
   socket.addListener("message", function(json) {
     subs = JSON.parse(json);
+    log(json);
     // When asked to subscribe to a feed_url
     ws_server.send(socket.id, "Subscribing to " + subs.feed_url);
     var subscription = subscriptions_store.subscribe(socket.id, subs.feed_url);
     subscribe(subscription.feed, "subscribe", subs.hub_url, function() {
-      log("Subscribed to " + subscription.feed_url + " for " + socket.id);
       ws_server.send(socket.id, "Subscribed to " + subs.feed_url);
+      log("Subscribed to " + subscription.feed.url + subs.feed_url + " for " + socket.id);
     }, function(error) {
-      log("Failed subscription to " + subs.feed_url + " for " + socket.id);
       ws_server.send(socket.id, "Couldn't subscribe to " + subs.feed_url + " : "+ error.trim() );
+      log("Failed subscription to " + subs.feed_url + " for " + socket.id);
     });
   });
 });
@@ -190,6 +212,7 @@ ws_server.addListener("connection", function(socket ) {
 // Handle Web Sockets when they disconnect. 
 ws_server.addListener("close", function(socket ) {
   // Not much to do.
+  //unsubscribe !!
 });
 
 // Web Server -------- (server <-> hub) --------------------------------------------
@@ -197,12 +220,30 @@ var web_server = express.createServer();
 
 // PubSubHubbub verification of intent
 web_server.get(config.pubsubhubbub.callback_url_path + ':feed_id', function(req, res) {
+//web_server.get(config.pubsubhubbub.callback_url_path + ':feed_id?hub.challenge?hub.topic=?hub.mode?hub.lease_seconds', function(req, res) {
+//web_server.get(config.pubsubhubbub.callback_url_path + ':feed_id?hub.challenge=challenge&hub.topic=topic&hub.mode=mode&hub.lease_seconds=secs', function(req, res) {
+    log("in server.get");
+    log("req.url"+req.url);
+    var challenge = req.url.split("hub.challenge=")[1].split("&")[0];
+    log("challenge"+challenge);
+    var mode = req.url.split("hub.mode=")[1].split("&")[0];
+    log("mode"+mode);
+    log("req.method"+req.method);
+    log("req.headers.accept"+req.headers.accept);
+    log("req.headers.server"+req.headers.server);
+    //log("req.params.hub_challenge"+req.params.hub_challenge);
+    //log("req.params[hub.challenge]"+req.params['hub.challenge']);
+    //log("req.params.hub.challenge"+req.params.hub.challenge);
     var feed = subscriptions_store.feeds[req.params.feed_id];
-    if (feed && req.query.hub.mode == "subscribe") {
-      log("Confirmed " + req.query.hub.mode + " to " + feed.feed.url )
-      res.send(req.query.hub.challenge, 200);
+    log("req.params.feed_id:"+ req.params.feed_id);
+    //if (feed && req.query.hub.mode == "subscribe") {
+    if (feed && mode == "subscribe") {
+      log("Confirmed " + mode + " to " + feed.feed.url )
+      res.send(challenge, 200);
+      log("Send: " + challenge);
+      //notify the publisher about the follower!
     }
-    else if (feed && req.query.hub.mode == "unsubscribe") {
+    else if (feed && mode == "unsubscribe") {
       // We need to check all the susbcribers. To make sure they're all offline
       var sockets = 0;
       for(subscription_id in feed.subscriptions) {
@@ -216,16 +257,16 @@ web_server.get(config.pubsubhubbub.callback_url_path + ':feed_id', function(req,
       if(sockets == 0) {
         // We haven't found any socket to send the updates too
         // Let's delete the feed
-        log("Confirmed " + req.query.hub.mode + " to " + feed.feed.url)
-        res.send(req.query.hub.challenge, 200);
+        log("Confirmed " + mode + " to " + feed.feed.url)
+        res.send(challenge, 200);
       }
       else {
-        log("Couldn't confirm " + req.query.hub.mode + " to " + feed.feed.url)
+        log("Couldn't confirm " + mode + " to " + feed.feed.url)
         res.send(404);
       }
     }
     else {
-      log("Couldn't confirm " + req.query.hub.mode + " to " + req.params.feed_id)
+      log("Couldn't confirm " + mode + " to " + req.params.feed_id)
       res.send(404);
     }
 });
@@ -234,19 +275,39 @@ web_server.get(config.pubsubhubbub.callback_url_path + ':feed_id', function(req,
 // Incoming POST notifications.
 // Sends the data to the right Socket, based on the subscription. Unsubscibes unused subscriptions.
 web_server.post(config.pubsubhubbub.callback_url_path + ':feed_id', function(req, res) {
+  log("in server post");
   var feed_subs = subscriptions_store.feeds[req.params.feed_id];
+  log("req.params.feed_id " + req.params.feed_id);
   if (feed_subs) {
     req.on('data', function(data) {
+      log("in data");
       var sockets = 0;
       for(subscription_id in feed_subs.subscriptions) {
         subscription = feed_subs.subscriptions[subscription_id];
         ws_server.send(subscription.socket_id, data, function(socket) {
           if(socket) {
             sockets += 1;
-            log("Sent notification for " + subscription.socket_id + " for " + subscription.feed.url)
+            log("Sent notification for " + subscription.socket_id + " for " + subscription.feed.url);
+            //socket die here: Sent notification for 58010 for http://smob.rhizomatik.net/me/rss
+
+//http.js:542
+//    throw new Error("Can't use mutable header APIs after sent.");
+//          ^
+//Error: Can't use mutable header APIs after sent.
+//    at ServerResponse.getHeader (http.js:542:11)
+//    at ServerResponse.header (/usr/local/lib/node/.npm/express/2.0.0/package/lib/response.js:226:17)
+//    at ServerResponse.send (/usr/local/lib/node/.npm/express/2.0.0/package/lib/response.js:65:17)
+//    at IncomingMessage.<anonymous> (/home/duy/socket-sub/server.js:296:13)
+//    at IncomingMessage.emit (events.js:64:17)
+//    at HTTPParser.onBody (http.js:121:23)
+//    at Socket.ondata (http.js:1001:22)
+//    at Socket._onReadable (net.js:675:27)
+//    at IOWatcher.onReadable [as callback] (net.js:177:10)
+
           } 
           else {
             log("Looks like " + subscription.socket_id + " is offline! Removing " + subscription.feed.url + " from it");
+            //have to subscribe each time socket is not connected! :(
             subscriptions_store.delete_subscription(subscription.feed.id, subscription.socket_id);
           }
         });
